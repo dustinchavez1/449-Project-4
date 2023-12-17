3c7a7e828ce13336210d9f2b94f13a2f0b0e0760
Adrian Charbonneau <109932327+adrianc4@users.noreply.github.com>
2023-12-16 18:40:11 -0800

    Fixed compiler errors

--- api/services/notifications/subscribe.py
+++ api/services/notifications/subscribe.py
@@ -7,16 +7,16 @@ router = APIRouter()
 
 redis_connection_pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
 
-@router.post(/subscribe/add_course)
+@router.post("/subscribe/add_course")
 def subscribe_to_new_course(
-    student_id: Depends(validate_student_id)
-    section_id: Depends(validate_section_id)
-    email: str
+    student_id: Depends(validate_student_id),
+    section_id: Depends(validate_section_id),
+    email: str,
     web_hook: str
 ):
     return
 
-@router.get(/subscribe/list_notifications)
+@router.get("/subscribe/list_notifications")
 def list_subscriptions(
     student_id: Depends(validate_student_id)
 ):
@@ -24,10 +24,10 @@ def list_subscriptions(
     subscriptions = get_subscriptions(student_id)
     return {"Subscriptions": subscriptions}
 
-@router.delete(/subscribe/delete_notification)
+@router.delete("/subscribe/delete_notification")
 def delete_subscriptions(
-    student_id: Depends(validate_student_id)
+    student_id: Depends(validate_student_id),
     section_id: Depends(validate_section_id)
 ):
-    response = delete_subscription(student_id,section_id)
-    return {f'Element removed: {response}'}
+    response = delete_subscription(student_id, section_id)
+    return {"Element removed": response}
